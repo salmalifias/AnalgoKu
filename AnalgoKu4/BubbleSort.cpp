69e4fb226a97ecaa12485c19e040a2961de20e5a
salmalifias <salmalifia.shafira@gmail.com>
2020-03-24 14:30:26 +0700

    tugas4

---
+++ AnalgoKu4/BubbleSort.cpp
@@ -0,0 +1,34 @@
+/*
+Nama	: Salma Alifia Shafira
+Kelas	: B
+NPM 	: 140810180058
+*/
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	int arr[100],n,temp;
+	cout << "\n+++++++++++++++++++++++++++++++++++++++++++++" <<endl;
+	cout<<"Massukan banyak elemen yang akan diinputkan : ";cin>>n;
+	cout << "\n+++++++++++++++++++++++++++++++++++++++++++++" << endl;
+	
+	for(int i=0;i<n;++i){
+		cout<<"Masukkan Elemen ke-"<<i+1<<" : ";cin>>arr[i];	}
+	
+	for(int i=1;i<n;i++){
+		for(int j=0;j<(n-1);j++){
+			if(arr[j]>arr[j+1]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;	}
+		}
+	}
+	cout << "\n+++++++++++++++++++++++++++++++++++++++++++++"  << endl;
+	cout<<"\nHasil Bubble Sort : "<<endl;
+	for(int i=0;i<n;i++){
+		cout<<" "<<arr[i];
+	}
+	cout << "\n+++++++++++++++++++++++++++++++++++++++++++++" <<endl;
+}
---
+++ AnalgoKu4/InsertionSort.cpp
@@ -0,0 +1,47 @@
+/*
+Nama	: Salma Alifia Shafira
+Kelas	: B
+NPM 	: 140810180058
+*/
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int data[100],data2[100],n;
+
+void insertion_sort()
+{
+	int temp,i,j;
+	for(i=1;i<=n;i++){
+	    temp = data[i];
+		j = i -1;
+	    while(data[j]>temp && j>=0){
+			data[j+1] = data[j];
+	   	    j--;
+	    }
+	    data[j+1] = temp;
+	}
+}
+int main()
+{
+	cout << "\n++++++++++++++++++++++++++++++++++" <<endl;
+	cout<<"Masukkan Jumlah Data : "; cin>>n;
+	cout<<endl;
+	cout << "\n++++++++++++++++++++++++++++++++++" << endl;
+	for(int i=1;i<=n;i++)
+	{
+	  cout<<"Masukkan data ke-"<<i<<" : ";
+	  cin>>data[i];
+	  data2[i]=data[i];
+	}
+	cout << "\n++++++++++++++++++++++++++++++++++" << endl;
+	insertion_sort();
+	cout<<"\ndata setelah sort : "<<endl;
+	for(int i=1; i<=n; i++)
+	{
+	  cout<<data[i]<<" ";
+	}
+	cout << "\n++++++++++++++++++++++++++++++++++"<<endl;
+	getch();
+}
